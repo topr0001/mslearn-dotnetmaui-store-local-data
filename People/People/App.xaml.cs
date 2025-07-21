f9249cf72fa1e34b1843949a86f5390fe483950f
topr0001 <topr0001@algonquinlive.com>
2025-07-20 21:35:25 -0400

    Exercise 1 Completed

--- People/People/App.xaml.cs
+++ People/People/App.xaml.cs
@@ -2,17 +2,16 @@
 
 public partial class App : Application
 {
-	public static PersonRepository PersonRepo { get; private set; }
+    public static PersonRepository PersonRepo { get; private set; }
 
-	public App()
-	{
-		InitializeComponent();
-		// TODO: Initialize the PersonRepository property with the PersonRespository singleton object
+    public App(PersonRepository repo)
+    {
+        InitializeComponent();
+        PersonRepo = repo;
+    }
 
-	}
-
-	protected override Window CreateWindow(IActivationState activationState)
-	{
-		return new Window(new AppShell());
-	}
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        return new Window(new AppShell());
+    }
 }
--- People/People/Models/Person.cs
+++ People/People/Models/Person.cs
@@ -1,4 +1,13 @@
-﻿namespace People.Models;
+﻿using SQLite;
+
+namespace People.Models;
+
+[Table("people")]
 public class Person
 {
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+
+    [MaxLength(250), Unique]
+    public string Name { get; set; }
 }
