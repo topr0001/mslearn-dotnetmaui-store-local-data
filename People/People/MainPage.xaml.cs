4e5d37d74991a0d6890d0694ee1dc63302b357ef
topr0001 <topr0001@algonquinlive.com>
2025-07-20 21:46:29 -0400

    Exercise 2 Completed

--- People/People/MainPage.xaml.cs
+++ People/People/MainPage.xaml.cs
@@ -5,27 +5,22 @@ namespace People;
 
 public partial class MainPage : ContentPage
 {
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
-
-    public void OnNewButtonClicked(object sender, EventArgs args)
+    public async void OnNewButtonClicked(object sender, EventArgs args)
     {
         statusMessage.Text = "";
-
-        App.PersonRepo.AddNewPerson(newPerson.Text);
+        await App.PersonRepo.AddNewPerson(newPerson.Text);
         statusMessage.Text = App.PersonRepo.StatusMessage;
     }
 
-    public void OnGetButtonClicked(object sender, EventArgs args)
+    public async void OnGetButtonClicked(object sender, EventArgs args)
     {
         statusMessage.Text = "";
-
-        List<Person> people = App.PersonRepo.GetAllPeople();
+        List<Person> people = await App.PersonRepo.GetAllPeople();
         peopleList.ItemsSource = people;
     }
-
 }
-
--- People/People/PersonRepository.cs
+++ People/People/PersonRepository.cs
@@ -1,5 +1,6 @@
 ﻿using SQLite;
 using People.Models;
+using System.Threading.Tasks;
 
 namespace People;
 
@@ -7,15 +8,15 @@ public class PersonRepository
 {
     string _dbPath;
     public string StatusMessage { get; set; }
-    private SQLiteConnection conn;
+    private SQLiteAsyncConnection conn;
 
-    private void Init()
+    private async Task Init()
     {
         if (conn != null)
             return;
 
-        conn = new SQLiteConnection(_dbPath);
-        conn.CreateTable<Person>();
+        conn = new SQLiteAsyncConnection(_dbPath);
+        await conn.CreateTableAsync<Person>();
     }
 
     public PersonRepository(string dbPath)
@@ -23,18 +24,18 @@ public class PersonRepository
         _dbPath = dbPath;
     }
 
-    public void AddNewPerson(string name)
+    public async Task AddNewPerson(string name)
     {
         int result = 0;
         try
         {
-            Init();
+            await Init();
 
             if (string.IsNullOrEmpty(name))
                 throw new Exception("Valid name required");
 
-            result = conn.Insert(new Person { Name = name });
-            StatusMessage = string.Format("{0} record(s) added (Name: {1})", result, name);
+            result = await conn.InsertAsync(new Person { Name = name });
+            StatusMessage = string.Format("{0} record(s) added [Name: {1})", result, name);
         }
         catch (Exception ex)
         {
@@ -42,12 +43,12 @@ public class PersonRepository
         }
     }
 
-    public List<Person> GetAllPeople()
+    public async Task<List<Person>> GetAllPeople()
     {
         try
         {
-            Init();
-            return conn.Table<Person>().ToList();
+            await Init();
+            return await conn.Table<Person>().ToListAsync();
         }
         catch (Exception ex)
         {
